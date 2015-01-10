056203a97ebb9fe02c70540240f723c3bdfa7646
Marek Zając <contact@zajacmarek.com>
2015-01-10 10:00:34 +0100

    move repository

---
+++ HoochAndPexNet.Commons/Config/Consts/ConfigScreenConsts.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HoochAndPexNet.Commons.Config.Consts
+{
+    public class ConfigScreenConsts
+    {
+        public static readonly string SCREEN_ROOT_ELEMENT = "screen";
+        public static readonly string SCREEN_WIDTH_ELEMENT = "screenWidth";
+        public static readonly string SCREEN_HEIGHT_ELEMENT = "screenHeight";
+        public static readonly string SCREEN_COLOR_DEPTH_ELEMENT = "colorDepth";
+    }
+}
